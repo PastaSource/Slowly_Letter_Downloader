5a51ce3c79fc37cc98e47094247e875ee5d3690c
Aaron <aaron.newbigging@gmail.com>
2022-08-28 23:20:10 +0100

    Various fixes improvements and bundled modified Selenium 4.2.0
    Will now create dir with penpals name and moved saved PDFs into there.
    Currently it only checks for SLOWLY.pdf in the downloads(letters)
    folder, this will stops crashes if there is a left over SLOWLY.pdf, but
    will probably still crash if it tries to move and then rename to an
    existing file.
    
    I could only get this code to function properly with Selenium version
    4.2.0. This isn't ideal, but I've bundled the used version with the
    code to make life easier. This version has been slightly modified to
    remove and error which would prevent Chrome from printing more than
    one pdf.

--- main.py
+++ main.py
@@ -12,18 +12,20 @@ from selenium.webdriver import Chrome, ChromeOptions
 
 dir_path = os.getcwd()
 download_path = os.path.join(dir_path, "letters")
-# chrome_path = os.path.join(dir_path, "chromium\\app\\Chrome-bin\\chrome.exe")
+chrome_path = os.path.join(dir_path, "chromium\\app\\Chrome-bin\\chrome.exe")
 website = 'https://web.slowly.app/'
 home_url = 'https://web.slowly.app/home'
 current_url_regex = '([\w]*:\/\/[\w.]*\/)([\w]*)'
 friend_regex = '([\w]*:\/\/[\w.]*\/)(friend)(\/[\w\d]*)'
 signature_regex = '>(.*)<\/h5><p>(.* \d\d\d\d) .*<br>'
 dot_regex = '<button>\d*<\/button>'
+penpal_regex = '<span.*mt-1">(\w*)<\/span>'
 xpath = "//div[@class='col-6 col-xl-4 mb-3']" # Outer HTML
 signature_xpath = "//div[@class='media-body mx-3 mt-2']" # Finds name and date printed on letter
 dot_xpath = "//ul[@class='slick-dots']"
 next_button_xpath = "//button[@class='slick-arrow slick-next']"
 back_button_xpath = "//a[@class='no-underline link py-2 px-2 ml-n2 col-pixel-width-50 flip active']"
+penpal_xpath = "//div[@class='col-9 pt-2']"
 
 print_settings = {
     "recentDestinations": [{
@@ -44,12 +46,9 @@ options.add_experimental_option("prefs", {
     "savefile.default_directory": download_path, #Change default directory for downloads
     "download.default_directory": download_path, #Change default directory for downloads
     "download.prompt_for_download": False, #To auto download the file
-    # "download.open_pdf_in_system_reader": False,
     "download.directory_upgrade": True,
     "profile.default_content_setting_values.automatic_downloads": 1,
     "safebrowsing.enabled": True
-    # "profile.default_content_settings.popups": 0,
-    # "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
 })
 options.add_argument("--kiosk-printing")
 
@@ -60,7 +59,7 @@ wait = WebDriverWait(driver, 30)
 
 def scroll_down():
     try:
-        # scrolls through letters to load them
+        # Scrolls through letters to load them
         scroll_pause_time = 2
         page_height = driver.execute_script("return document.body.scrollHeight")
         while True:
@@ -85,8 +84,6 @@ def check_for_photos():
     else:
         return False
 
-
-
 def photo_amount():
     photos = driver.find_element(By.XPATH, dot_xpath)
     photos_innerhtml = photos.get_attribute('innerHTML')
@@ -94,25 +91,33 @@ def photo_amount():
     photo_count = len(search)
     return photo_count
 
-def make_pdf(letter_count):
-    print(f"Printing letter {letter_count}")
-    driver.execute_script("window.print()")
-    time.sleep(2)
-    # driver.print_page()
+def make_pdf(letter_count, penpal_dir):
     letter = driver.find_element(By.XPATH, signature_xpath)
     innerhtml = letter.get_attribute('innerHTML')
     username = re.search(signature_regex, innerhtml).group(1)
     date = re.search(signature_regex, innerhtml).group(2)
-    pdf_name = f"{username}_letter{letter_count}_{date}.pdf"
+    pdf_name = f"letter{letter_count}_{username}_{date}.pdf"
     file = "SLOWLY.pdf"
-    os.rename(f"{download_path}\\{file}", f"{download_path}\\{pdf_name}") # perhaps moved this into if exists block
-                                                                          # to prevent errors if file exists
-    if exists(f"{download_path}\\{pdf_name}"):
+
+    if exists(f"{download_path}\\{file}"):
+        os.remove(f"{download_path}\\{file}")
+    if exists(f"{penpal_dir}\\{file}"):
+        return print("Letter already exists! \nSkipping...")
+
+    # Prints letter as PDF with name "SLOWLY.pdf" in download_path
+    print(f"Printing letter {letter_count}")
+    driver.execute_script("window.print()")
+    time.sleep(2)
+
+    os.replace(f"{download_path}\\{file}", f"{penpal_dir}\\{file}") # Moves SLOWLY.pdf into penpal_dir
+    os.rename(f"{penpal_dir}\\{file}", f"{penpal_dir}\\{pdf_name}") # Renames SLOWLY.pdf to pdf_name var
+
+    if exists(f"{penpal_dir}\\{pdf_name}"):
         print(f"Letter {letter_count} successfully printed!")
     else:
         print(f"Letter {letter_count} failed to print.")
 
-def open_letter(letter_int, letter_count):
+def open_letter(letter_int, letter_count, penpal_dir):
     letters = driver.find_elements(By.XPATH, xpath)
     letter = letters[letter_int]
     letter.click()
@@ -134,8 +139,17 @@ def open_letter(letter_int, letter_count):
     # may be able to use isDisplay() method that returns a boolean if the image is displayed.
     # whether or not this will confirm if it is loaded or just being displayed, I'm not sure.
     # could use the driver.wait.until method for this (or whatever it is).
-    make_pdf(letter_count)
+    make_pdf(letter_count, penpal_dir)
     print("Going back to letters...")
+
+def mk_penpal_dir(penpal):
+    penpal_dir = os.path.join(download_path, penpal)
+    if exists(penpal_dir):
+        print("Penpal downloaded directory already exists in 'letters' folder.")
+    else:
+        print(f"Making download directory for {penpal}")
+        os.mkdir(penpal_dir)
+    return penpal_dir
 
 def main():
     while driver.current_url != home_url:
@@ -152,27 +166,23 @@ def main():
     finally:
         pass
 
+    # mkdir with name of penpal
+    penpal_name_obtain = driver.find_element(By.XPATH, penpal_xpath).get_attribute('innerHTML')
+    penpal = re.search(penpal_regex, penpal_name_obtain).group(1)
+    penpal_dir = mk_penpal_dir(penpal)
+
     letters = driver.find_elements(By.XPATH, xpath)
-    current_letter_int = len(letters)
-    amount_letters = current_letter_int
+    current_letter_int = len(letters) # tracks which letter is currently being processed
+    amount_letters = current_letter_int # amount of letters available to download
     for letter in range(0, amount_letters):
-        open_letter(letter, current_letter_int)
+        open_letter(letter, current_letter_int, penpal_dir)
         current_letter_int -= 1
         back_button = driver.find_element(By.XPATH, back_button_xpath)
         back_button.click()
         time.sleep(2)
-    # for letter in letters:
-    #     print(f"Letter {current_letter_int} loaded...")
-    #     open_letter(letter, current_letter_int)
-    #     current_letter_int -= 1
-    #     back_button = driver.find_element(By.XPATH, back_button_xpath)
-    #     back_button.click()
-    #     time.sleep(2)
-    #     letters = driver.find_elements(By.XPATH, xpath)
-    # else:
-    #     print(f"{amount_letters} letters successfully printed!")
-
-
+    else:
+        print(f"{amount_letters} letters successfully printed!")
+        driver.quit()
 
 if __name__ == '__main__':
     main()
